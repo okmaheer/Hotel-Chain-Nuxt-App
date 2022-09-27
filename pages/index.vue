b7bc778609e2f19669af7c7e3ebee79d50440496
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-09-28 00:55:22 +0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div
+      v-for="home in homes"
+      :key="home.objectID"
+      style="float: left; margin: 10px"
+    >
+      <HomeCard :home="home" />
+    </div>
+  </div>
 </template>
 
 <script>
+import homes from "~/data/homes";
+
 export default {
-  name: 'IndexPage'
-}
+  head(){
+  return{
+    title: 'Homepage',
+    meta:[
+      {
+        name:'description',
+        content:'This is a homepage!',
+        hid:'description'
+      }
+    ]
+  }
+},
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
 </script>
