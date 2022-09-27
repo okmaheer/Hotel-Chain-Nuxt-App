1d963a91f07cf488761d31121471ca0e9b70f2d8
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-09-28 00:55:19 +0500

    Create _id.vue

---
+++ pages/home/_id.vue
@@ -0,0 +1,24 @@
+ <template>
+        <div>
+    <img :src="home.images[0]" style="width:200px"><br/>
+   {{ home.title }}<br/>
+   {{ home.location.address }} {{ home.location.city }} {{  home.location.state }} <br/>
+   {{ home.guests }} guests <br/>
+   {{ home.pricePerNight }} /night <br/>
+  
+  </div>
+ </template>
+ <<script>
+import homes from "~/data/homes";
+ export default {
+    data(){
+        return {
+            home:{},
+        }
+    },
+  created(){
+   this.home = homes.find((home) => home.objectID == this.$route.params.id)
+
+  } 
+ }
+ </script>   
