679fa3b0c9d1370fc98ee48fff8a70cb7223ab29
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-09-28 17:34:59 +0500

    Update _id.vue

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,16 +1,24 @@
  <template>
-        <div>
-    <img :src="home.images[0]" style="width:200px"><br/>
-   {{ home.title }}<br/>
-   {{ home.location.address }} {{ home.location.city }} {{  home.location.state }} <br/>
-   {{ home.guests }} guests <br/>
-   {{ home.pricePerNight }} /night <br/>
-  
+  <div>
+    <div style="display: flex">
+      <img v-for="image in home.images" :key="image" :src="image" style="width: 200px" />
+    </div>
+    {{ home.title }}<br/>
+    {{ home.pricePerNight }} /night <br/>
+     <img src="/images/marker.svg" width="20" height="20"  />{{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}
+     <img src="/images/star.svg" width="20" height="20"  />{{ home.reviewValue }} <br />
+     {{ home.guests }} guests     {{ home.bedrooms }} rooms     {{ home.beds }} beds     {{ home.bathrooms }} bath<br/>
+ 
   </div>
- </template>
+</template>
  <<script>
 import homes from "~/data/homes";
  export default {
+    head(){
+        return {
+        title:this.home.title,
+        }
+    },
     data(){
         return {
             home:{},
