98e6a574b204c45d528d03ecf41168aafb6156f1
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-10-18 18:00:33 +0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,8 +24,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [ '~plugins/maps.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
