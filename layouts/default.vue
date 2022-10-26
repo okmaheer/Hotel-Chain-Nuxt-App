1d06738e715ee46c235e0fb6c55be0e43dbcd593
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-10-26 19:38:41 +0500

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,16 @@
 <template>
     <div>
-     <header>
+     <header style="background-color:#eee;">
         <nuxt-link to="/"> HOME </nuxt-link>
+        <input type="text" ref="citySearch">
      </header>
         <nuxt/>
     </div>
 </template>
+<script>
+export default {
+  mounted() {
+    this.$maps.makeAutoComplete(this.$refs.citySearch);
+  },
+};
+</script>
