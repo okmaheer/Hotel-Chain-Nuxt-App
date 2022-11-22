25e480f5436aba99759473a5fbf2d287c1fe1eb7
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-11-22 18:23:16 +0500

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,36 @@
 <template>
-    <div>
-     <header style="background-color:#eee;">
-        <nuxt-link to="/"> HOME </nuxt-link>
-        <input type="text" ref="citySearch">
-     </header>
-        <nuxt/>
-    </div>
+  <div>
+      <header>
+          <nuxt-link to="/">Home</nuxt-link>
+          <input type="text" ref="citySearch" @changed="changed">
+      </header>
+      <nuxt/>
+  </div>
 </template>
 <script>
-export default {
-  mounted() {
-    this.$maps.makeAutoComplete(this.$refs.citySearch);
+export default{
+  mounted(){
+      this.$maps.makeAutoComplete(this.$refs.citySearch)
   },
-};
+  methods:{
+      changed(event){
+          const place = event.detail
+          if(!place.geometry)return
+          
+          this.$router.push({
+              name:'search',
+              query:{
+                  lat: place.geometry.location.lat(),
+                  lng: place.geometry.location.lng(),
+                  label: this.$refs.citySearch.value,
+
+              }
+          })
+      }
+
+
+  }
+  
+}
+
 </script>
