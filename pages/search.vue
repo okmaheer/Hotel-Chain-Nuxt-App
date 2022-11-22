243a9189578f9952c872671f7b54db6baf1307d9
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-11-22 18:36:37 +0500

    Update search.vue

--- pages/search.vue
+++ pages/search.vue
@@ -1,3 +1,27 @@
-
-
-
+<template>
+    <div>{{ lat }}/{{ lng }} / {{ label }}<br />
+        <div v-for="home in homes" :key="home.objectID">{{ home.title }}<br /></div>
+    </div>
+</template>
+<script>
+export default {
+    async beforeRouteUpdate(to,from,next){
+        const data = await $dataApi.getHomesByLocation(to.query.lat,to.query.lng)
+        console.log(data)
+        this.homes = data.json.hits
+        this.label =to.query.label
+        this.lat = to.query.lat
+        this.lng = to.query.lng
+        next()
+    },
+    async asyncData({ query, $dataApi }) {
+        const data = await $dataApi.getHomesByLocation(query.lat, query.lng)
+        return {
+            homes: data.json.hits,
+            label: query.label,
+            lat: query.lat,
+            lng: query.lng,
+        }
+    }
+}
+</script>
