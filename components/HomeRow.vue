6a64782754d69891d2abb42696d85a8fc06870b0
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-11-23 13:52:09 +0500

    Update HomeRow.vue

--- components/HomeRow.vue
+++ components/HomeRow.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>
-    <img :src="home.images[0]" style="width:200px"><br/>
+    <div style="clear:left;">
+    <img :src="home.images[0]" style="width:200px;float:left;padding:10px;"><br/>
    {{ home.title }}<br/>
    {{ home.location.address }} {{ home.location.city }} {{  home.location.state }} <br/>
-   {{ home.guests }} guests <br/>
+   {{ home.guests }} guests,{{ home.bedrooms }} rooms, {{ home.beds }} beds,{{ home.bathrooms  }} baths <br/>
    {{ home.pricePerNight }} /night <br/>
-  
+   <img src="/images/star.svg" width="20" height="20" />{{ home.reviewValue }} ({{ home.reviewCount }}) <br/>
   </div>
 </template>
 <script>
