9c52eb6e84ac2f0ef44feba7a6b9d480d255fd3f
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-11-23 17:25:34 +0500

    Update search.vue

--- pages/search.vue
+++ pages/search.vue
@@ -1,25 +1,48 @@
 <template>
-    <div>{{ lat }}/{{ lng }} / {{ label }}<br />
-        {{ homes }}
-        <div v-if="homes.lenght > 0">
-        <HomeRow v-for="home in homes" :key="home.objectID" :home="home"/>
+    <div>Result for {{ label }}<br />
+        <div style="height:800px;width:800px;float:right;" ref="map"></div>
+        <div v-if="Object.keys(homes).length > 0">
+            <nuxt-link v-for="home in homes" :key="home.objectID" :to="`/home/${home.objectID}`">
+            <HomeRow :home="home" />
+            </nuxt-link>
         </div>
         <div v-else> No Result Found</div>
     </div>
- 
+
 </template>
 <script>
 export default {
-    head(){
-       return 
-       title: `Homes around ${this.label}`
+    head() {
+        return {
+            title: `Homes around ${this.label}`
+        }
     },
-    async beforeRouteUpdate(to,from,next){
-        const data = await this.$dataApi.getHomesByLocation(to.query.lat,to.query.lng)
+    mounted() {
+        this.updateMap()
+    },
+    methods: {
+        updateMap() {
+            this.$maps.showMap(this.$refs.map, this.lat, this.lng,this.getHomeMarkers())
+        },
+        getHomeMarkers(){
+      if(this.homes.length ==0) return null
+
+      return this.homes.map((home) => {
+        return {
+          ...home._geoloc,
+          pricePerNight: home.pricePerNight,
+          id: home.objectID,
+        }
+      })
+    }
+    },
+    async beforeRouteUpdate(to, from, next) {
+        const data = await this.$dataApi.getHomesByLocation(to.query.lat, to.query.lng)
         this.homes = data.json.hits
-        this.label =to.query.label
+        this.label = to.query.label
         this.lat = to.query.lat
         this.lng = to.query.lng
+        this.updateMap()
         next()
     },
     async asyncData({ query, $dataApi }) {
@@ -33,3 +56,17 @@ export default {
     }
 }
 </script>
+<style>
+.marker {
+  background-color: white;
+  border: 1px solid lightgray;
+  font-weight: bold;
+  border-radius: 20px;
+  padding: 5px 8px;
+}
+.marker-highlight {
+  color: white !important;
+  background-color: black;
+  border-color:black;
+}
+</style>
