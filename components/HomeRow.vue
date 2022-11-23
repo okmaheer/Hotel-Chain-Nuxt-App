0d07eca585b70ff851bc1a4ae4db0a5ebc7fbdbc
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-11-23 14:29:05 +0500

    Update HomeRow.vue

--- components/HomeRow.vue
+++ components/HomeRow.vue
@@ -3,7 +3,7 @@
     <img :src="home.images[0]" style="width:200px;float:left;padding:10px;"><br/>
    {{ home.title }}<br/>
    {{ home.location.address }} {{ home.location.city }} {{  home.location.state }} <br/>
-   {{ home.guests }} guests,{{ home.bedrooms }} rooms, {{ home.beds }} beds,{{ home.bathrooms  }} baths <br/>
+   {{ pluralize(home.guests,"guest") }} ,{{ pluralize(home.bedrooms,"room") }}, {{ pluralize(home.beds,"bed") }} ,{{ pluralize(home.bathrooms,"bath")  }}  <br/>
    {{ home.pricePerNight }} /night <br/>
    <img src="/images/star.svg" width="20" height="20" />{{ home.reviewValue }} ({{ home.reviewCount }}) <br/>
   </div>
@@ -15,6 +15,13 @@ export default {
     home:{
       type:Object,
       required:true,
+    }
+  },
+  methods:{
+    pluralize(number,singularWord){
+      const text = `${number} ${singularWord}`
+      if(number == 1 )return text
+       return text + 's'
     }
   }
 }
