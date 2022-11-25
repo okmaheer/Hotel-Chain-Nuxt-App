af4f12df4830d4157ed99edf2626523f2f416ca6
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-11-25 18:53:29 +0500

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,52 @@
 <template>
-  <div>
-      <header>
-          <nuxt-link to="/">Home</nuxt-link>
-          <input type="text" ref="citySearch" @changed="changed">
-      </header>
-      <nuxt/>
-  </div>
+    <div class="app">
+        <header class="app-header">
+         <div class="app-logo">
+            <img src="/images/logo.svg" alt="">
+         </div>
+         <div class="app-search">
+            <input type="text" ref="citySearch" @changed="changed" placeholder="Enter your address" />
+            <input type="text" class="datepicker" placeholder="Check in" />
+            <input type="text" class="datepicker" placeholder="Check out" />
+            <button>
+                <img src="/images/icons/search.svg" />
+            </button>
+
+         </div>
+        </header>
+
+
+        <header>
+            <nuxt-link to="/">Home</nuxt-link>
+            <input type="text" ref="citySearch" @changed="changed">
+        </header>
+        <nuxt />
+    </div>
 </template>
 <script>
-export default{
-  mounted(){
-      this.$maps.makeAutoComplete(this.$refs.citySearch)
-  },
-  methods:{
-      changed(event){
-          const place = event.detail
-          if(!place.geometry)return
-          
-          this.$router.push({
-              name:'search',
-              query:{
-                  lat: place.geometry.location.lat(),
-                  lng: place.geometry.location.lng(),
-                  label: this.$refs.citySearch.value,
+export default {
+    mounted() {
+        this.$maps.makeAutoComplete(this.$refs.citySearch)
+    },
+    methods: {
+        changed(event) {
+            const place = event.detail
+            if (!place.geometry) return
 
-              }
-          })
-      }
+            this.$router.push({
+                name: 'search',
+                query: {
+                    lat: place.geometry.location.lat(),
+                    lng: place.geometry.location.lng(),
+                    label: this.$refs.citySearch.value,
+
+                }
+            })
+        }
 
 
-  }
-  
+    }
+
 }
 
 </script>
