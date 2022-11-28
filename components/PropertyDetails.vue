2a17d3f4d96e11876fabd48814f7a4300b66b4c0
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-11-28 17:24:46 +0500

    property detail set

---
+++ components/PropertyDetails.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="app-wrapper">
+        <div class="app-double-column app-property-details">
+            <div>
+                <h1>{{ home.title }}</h1>
+                <div class="app-flex">
+                    <div class="app-address">
+                        {{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{
+                                home.location.country
+                        }}
+                    </div>
+                    <div class="app-rating">
+                        {{ home.reviewValue }} <span>({{ home.reviewCount }})</span>
+                    </div>
+                </div>
+                <div class="app-property-details-footer">
+                    {{ pluralize(home.guests, "Guest") }} &middot;
+                    {{ pluralize(home.bedrooms, "Room") }} &middot;
+                    {{ pluralize(home.beds, "Bed") }} &middot;
+                    {{ pluralize(home.bathrooms, "Bath") }}
+                </div>
+            </div>
+            <div>
+                <div class="app-price">
+                    ${{ home.pricePerNight }}<span> / night</span>
+                </div>
+                <div class="app-search">
+                    <input type="text" class="datepicker" placeholder="Check in" />
+                    <input type="text" class="datepicker" placeholder="Check out" />
+                </div>
+
+                <button class="app-big-button" @click="checkout">Request to book!</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import pluralize from '~/utils/pluralize'
+
+export default {
+    props: {
+        home: {
+            type: Object,
+            required: true,
+        }
+    },
+    methods: {
+        pluralize,
+    },
+    data() {
+        return {
+            range: {
+                start: null,
+                end: null,
+            }
+        }
+    }
+}
+</script>
