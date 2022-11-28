5a3c11e3add6eb79e720260d720e4850267146ff
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-11-28 17:26:42 +0500

    update property page

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,74 +1,36 @@
  <template>
-  <div>
-    <div style="display: flex">
-      <img
-        v-for="image in home.images"
-        :key="image"
-        :src="image"
-        style="width: 200px"
-      />
-    </div>
-    {{ home.title }}<br />
-    {{ home.pricePerNight }} /night <br />
-    <img src="/images/marker.svg" width="20" height="20" />{{
-      home.location.address
-    }}
-    {{ home.location.city }} {{ home.location.state }}
-    {{ home.location.country }}
-    <img src="/images/star.svg" width="20" height="20" />{{ home.reviewValue }}
-    <br />
-    {{ home.guests }} guests {{ home.bedrooms }} rooms {{ home.beds }} beds
-    {{ home.bathrooms }} bath<br />
-    {{ home.description }}
-    <div style="height:800px;width:800px;" ref="map">
-     
-    </div>
-
-    <div v-for="review in reviews" :key="review.objectID">
-      <img :src="review.reviewer.image" width="50" height="50" /><br />
-      {{ review.reviewer.name }}<br />
-      {{ formatDate(review.date) }}<br />
-      <short-text :text="review.comment" :target="150" /><br />
-    </div>
-    <img :src="user.image" width="50" height="50" /><br />
-    {{ user.name }}<br />
-    {{ formatDate(user.joined) }}<br />
-    {{ user.reviewCount }}<br />
-    {{ user.description }}<br />
+  <div class="app-container">
+    <PropertyGallery :images="home.images" />
+    <PropertyDetails :home="home" />
+    <PropertyDescription :home="home"/>
+    <PropertyMap :home="home"/>
+    <PropertyReviews :reviews="reviews"/>
+    <PropertyHost :user="user "/>
+   
   </div>
 </template>
  <script>
  export default {
-    head(){
+    head() {
         return {
-        title:this.home.title,     
-        }
+            title: this.home.title,
+        };
     },
-    mounted(){
-      console.log(this.$maps);
-      this.$maps.showMap(this.$refs.map,this.home._geoloc.lat,this.home._geoloc.lng)
-    },
-    async asyncData({params,$dataApi}){
-      const responses = await Promise.all([
-      await $dataApi.getHome(params.id),
-      await $dataApi.getReviewsByHomeId(params.id),
-      await $dataApi.getUserByHomeId(params.id) 
-      ])
 
-      const badResponse = responses.find((response)=> !response.ok)
-      if(badResponse) return ({statusCode:badResponse.status, messgae: badResponse.statusText})
-   
-       return {
-        home: responses[0].json,
-        reviews: responses[1].json.hits,
-        user: responses[2].json.hits[0],
-      }
-  }, 
-  methods:{
-     formatDate(dateStar){
-     const date = new Date(dateStar)
-     return date.toLocaleDateString(undefined,{ month:'long', year: 'numeric'})
-     }
+    async asyncData({ params, $dataApi }) {
+        const responses = await Promise.all([
+            await $dataApi.getHome(params.id),
+            await $dataApi.getReviewsByHomeId(params.id),
+            await $dataApi.getUserByHomeId(params.id)
+        ]);
+        const badResponse = responses.find((response) => !response.ok);
+        if (badResponse)
+            return ({ statusCode: badResponse.status, messgae: badResponse.statusText });
+        return {
+            home: responses[0].json,
+            reviews: responses[1].json.hits,
+            user: responses[2].json.hits[0],
+        };
+    },
 }
- }
  </script>    
