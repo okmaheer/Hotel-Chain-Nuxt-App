89620f298820b5d232d3e84c07cf772c6c1eced0
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-12-08 15:03:59 +0500

    Update mutations.js

--- store/auth/mutations.js
+++ store/auth/mutations.js
@@ -1,5 +1,10 @@
 export default {
     setIsLoggedIn(state, value) {
         state.isLoggedin = value
+    },
+    setUser(state, value){
+       
+      state.user = value
+      console.log(state)
     }
 }
