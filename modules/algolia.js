6dd307a4369194089b9b0445f92045dd46267da9
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-12-14 17:18:57 +0500

    Update algolia.js

--- modules/algolia.js
+++ modules/algolia.js
@@ -1,40 +1,63 @@
-import fetch from 'node-fetch';
-import { unWrap, getErrorResponse} from '../utils/fetchUtils'
-export default function () {
-    const algoliaConfig = this.options.publicRuntimeConfig.algolia
+import fetch from 'node-fetch'
+import { unWrap, getErrorResponse } from '../utils/fetchUtils'
+
+export default function(){
+    const algoliaConfig = this.options.privateRuntimeConfig.algolia
     const headers = {
-        'X-Algolia-API-key': algoliaConfig.key,
+        'X-Algolia-API-Key': algoliaConfig.key,
         'X-Algolia-Application-Id': algoliaConfig.appId,
     }
     this.nuxt.hook('render:setupMiddleware', (app) => {
         app.use('/api/user', getUserRoute)
     })
 
-    function getUserRoute(req, res, next) {
+    async function getUserRoute(req, res, next){
+        const identity = req.identity
+        const userData = await getUserById(identity)
+
+        if(userData.status == 200){
+            sendJSON(userData.json, res)
+            return
+        }
         createUser(req.identity)
-        next()
+        sendJSON(makeUserPayload(identity), res)        
     }
-   async function createUser(identity){
+    async function createUser(identity){
         try {
             return unWrap(await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
-              headers,
-              method: 'PUT',
-              body: JSON.stringify(makeUserPayload(identity))
+                headers,
+                method: 'PUT',
+                body: JSON.stringify(makeUserPayload(identity))
             }))
-          } catch (error) {
+        } catch(error){
             return getErrorResponse(error)
-          }
+        }
     }
-    function  makeUserPayload(identity){
-    return {
-        name:identity.name,
-        email:identity.email,
-        image:identity.image,
-        homeId:[],
-        reviewCount:0,
-        description: '',
-        joined: new Date().toISOString()
+    async function getUserById(identity){
+        try {
+            return unWrap(await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
+                headers,               
+            }))
+        } catch(error){
+            return getErrorResponse(error)
+        }
     }
+
+    function sendJSON(data, res){
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(data))
+    }
+
+    function makeUserPayload(identity){
+        return {
+            name: identity.name,
+            email: identity.email,
+            image: identity.image,
+            homeId: [],
+            reviewCount: 0,
+            description: '',
+            joined: new Date().toISOString()
+        }
     }
 
 }
