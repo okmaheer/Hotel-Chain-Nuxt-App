21e26d81c9d7344eeaa1e5b545bfecd7a739101b
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-12-14 17:17:26 +0500

    Update actions.js

--- store/auth/actions.js
+++ store/auth/actions.js
@@ -1,5 +1,9 @@
 export default {
     IsLoggedIn({ state, commit },value) {
         commit('setIsLoggedIn',value)
+    },
+    setUser({ state, commit },value) {
+        console.log('setUse',value)
+        commit('setUser',value)
     }
 }
