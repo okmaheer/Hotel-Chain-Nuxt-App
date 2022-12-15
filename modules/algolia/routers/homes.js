fa7db6dc7931aa763f305f145f1e752acfc3863b
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-12-15 23:26:54 +0500

    home route added

---
+++ modules/algolia/routers/homes.js
@@ -0,0 +1,38 @@
+import { sendJSON,rejectHitBadRequest, hasBadBody } from '../helper'
+import { v4 as uuidv4 } from 'uuid'
+export default (apis) => {
+    
+    return async  (req, res, next) =>{
+        if(req.method == 'POST'){
+            if(hasBadBody(req)){
+                return rejectHitBadRequest(res)
+            }
+            await createHome(req.identity,req.body,res)
+
+            return
+        }
+        rejectHitBadRequest(res)          
+    }  
+
+     async function createHome(identity,body,res){
+         const homeId = uuidv4()
+         const payload = {
+            ...body,
+            reviewCount : 0,
+            reviewValue : 0,
+            userId: identity.id,
+
+         }
+         const resp = await apis.homes.create(homeId, payload)
+         if(!resp.ok){ 
+            res.statusCode = 500
+            res.send()
+            return
+         }
+         sendJSON({},res)
+          
+          
+     }
+
+   
+}
