819ead937bc357eee9be55c0452ac6a8224f08a8
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-12-15 19:41:30 +0500

    server algolia plus body parse added

---
+++ modules/algolia/index.js
@@ -0,0 +1,12 @@
+import getApis from './apis'
+import bodyParser from 'body-parser'
+import userRouter from './routers/user'
+export default function(){
+    const algoliaConfig = this.options.privateRuntimeConfig.algolia
+    const apis = getApis(algoliaConfig)
+    this.nuxt.hook('render:setupMiddleware', (app) => {
+        app.use(bodyParser.urlencoded())
+        app.use('/api/user', userRouter(apis))
+    })
+
+}
